50a83c04dd4f5999fdabe7b204ad440590192017
Jennifer Mann <jennifermann17@gmail.com>
2016-03-26 14:27:43 -0400

    moved tests to cypress folder + added cypres-cli command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,15 +1,14 @@
 // ***********************************************
-// This example spec_helper.js shows you how to
-// create the custom command: 'createDefaultTodos'.
+// This example commands.js shows you how to
+// create the custom command: 'login'.
 //
-// The spec_helper.js file is a great place to
-// add reusable logic / custom commands which
-// become usable in every single test file.
+// The commands.js file is a great place to
+// modify existing commands and create custom
+// commands for use throughout your tests.
 //
 // You can read more about custom commands here:
-// http://on.cypress.io/commands#customizing
+// https://on.cypress.io/api/commands
 // ***********************************************
-//
 
 Cypress.addParentCommand("createDefaultTodos", function(){
 
@@ -20,16 +19,9 @@ Cypress.addParentCommand("createDefaultTodos", function(){
   // begin the command here, which by will display
   // as a 'spinning blue state' in the UI to indicate
   // the command is running
-  var log = Cypress.Log.command({
+  var cmd = Cypress.Log.command({
     name: "create default todos",
     message: [],
-
-    // this is a temporary fix to prevent this log
-    // from auto-ending after the command is finished.
-    // some internal logic needs refactoring for this
-    // not to need to be specified.
-    autoEnd: false,
-
     onConsole: function(){
       // we're creating our own custom message here
       // which will print out to our browsers console
@@ -59,24 +51,17 @@ Cypress.addParentCommand("createDefaultTodos", function(){
       // above we want to return the .todo-list li's
       // to allow for further chaining and then
       // we want to snapshot the state of the DOM
-      // and end the log so it goes from that
-      // 'spinning blue state' to the 'finished state'.
-      //
-      // this behavior is also temporary as Cypress needs some
-      // internal refactoring to make it intelligent
-      // enough to automatically figure this out on its own.
-      // currently the log needs to be specifically ended
-      // when 'autoEnd' is set to false.
-      log.set({$el: $listItems}).snapshot().end()
+      // and end the command so it goes from that
+      // 'spinning blue state' to the 'finished state'
+      cmd.set({$el: $listItems}).snapshot().end()
     })
 })
 
 Cypress.addParentCommand("createTodo", function(todo){
 
-  var log = Cypress.Log.command({
+  var cmd = Cypress.Log.command({
     name: "create todo",
     message: todo,
-    autoEnd: false,
     onConsole: function(){
       return {
         "Inserted Todo": todo
@@ -100,6 +85,6 @@ Cypress.addParentCommand("createTodo", function(todo){
       // set the $el for the command so
       // it highlights when we hover over
       // our command
-      log.set({$el: $li}).snapshot().end()
+      cmd.set({$el: $li}).snapshot().end()
     })
 })
