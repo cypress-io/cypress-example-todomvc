edbe596937d020d32992f25b1e57346515642972
Jennifer Mann <jennifermann17@gmail.com>
2016-03-26 14:35:07 -0400

    reverted app_spec to latest version, removed example_spec

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -31,7 +31,7 @@ describe("TodoMVC - React [000]", function(){
     // before each test, which serves us the
     // TodoMVC App we want to test against
     //
-    // https://github.com/cypress-io/cypress/wiki/visit
+    // http://on.cypress.io/visit
     cy.visit("http://localhost:8888")
   })
 
@@ -40,7 +40,7 @@ describe("TodoMVC - React [000]", function(){
       // get the currently focused element and assert
       // that it has class="new-todo"
       //
-      // https://github.com/cypress-io/cypress/wiki/focused
+      // http://on.cypress.io/focused
       cy.focused().should("have.class", "new-todo")
     })
   })
@@ -52,8 +52,8 @@ describe("TodoMVC - React [000]", function(){
       // parse through. Instead we'll opt to use real selectors
       // so as to make our testing intentions as clear as possible.
       //
-      // https://github.com/cypress-io/cypress/wiki/commands#options
-      // https://github.com/cypress-io/cypress/wiki/get
+      // http://on.cypress.io/commands#options
+      // http://on.cypress.io/get
       cy
         .get(".todo-list li").should("not.exist")
         .get(".main").should("not.exist")
@@ -63,16 +63,16 @@ describe("TodoMVC - React [000]", function(){
 
   context("New Todo [005]", function(){
     // New commands used here:
-    // - cy.type     https://github.com/cypress-io/cypress/wiki/type
-    // - cy.eq       https://github.com/cypress-io/cypress/wiki/eq
-    // - cy.find     https://github.com/cypress-io/cypress/wiki/find
-    // - cy.contains https://github.com/cypress-io/cypress/wiki/contains
-    // - cy.should   https://github.com/cypress-io/cypress/wiki/should
-    // - cy.as       https://github.com/cypress-io/cypress/wiki/as
+    // - cy.type     http://on.cypress.io/type
+    // - cy.eq       http://on.cypress.io/eq
+    // - cy.find     http://on.cypress.io/find
+    // - cy.contains http://on.cypress.io/contains
+    // - cy.should   http://on.cypress.io/should
+    // - cy.as       http://on.cypress.io/as
 
     // New concepts introduced:
-    // - Aliasing    https://github.com/cypress-io/cypress/wiki/aliasing
-    // - Assertions  https://github.com/cypress-io/cypress/wiki/assertions
+    // - Aliasing    http://on.cypress.io/aliasing
+    // - Assertions  http://on.cypress.io/assertions
 
     it("should allow me to add todo items [006]", function(){
       cy
@@ -80,13 +80,13 @@ describe("TodoMVC - React [000]", function(){
         .get(".new-todo").type(TODO_ITEM_ONE).type("{enter}")
 
         // make sure the 1st label contains the 1st todo text
-        .get(".todo-list li").eq(0).find("label").contains(TODO_ITEM_ONE)
+        .get(".todo-list li").eq(0).find("label").should("contain", TODO_ITEM_ONE)
 
         // create 2nd todo
         .get(".new-todo").type(TODO_ITEM_TWO).type("{enter}")
 
         // make sure the 2nd label contains the 2nd todo text
-        .get(".todo-list li").eq(1).find("label").contains(TODO_ITEM_TWO)
+        .get(".todo-list li").eq(1).find("label").should("contain", TODO_ITEM_TWO)
     })
 
     it("should clear text input field when an item is added [007]", function(){
@@ -108,9 +108,9 @@ describe("TodoMVC - React [000]", function(){
         // `cy.contains` can verify this correctly
         .get(".todo-count").contains("3 items left")
 
-        .get("@todos").eq(0).find("label").contains(TODO_ITEM_ONE)
-        .get("@todos").eq(1).find("label").contains(TODO_ITEM_TWO)
-        .get("@todos").eq(2).find("label").contains(TODO_ITEM_THREE)
+        .get("@todos").eq(0).find("label").should("contain", TODO_ITEM_ONE)
+        .get("@todos").eq(1).find("label").should("contain", TODO_ITEM_TWO)
+        .get("@todos").eq(2).find("label").should("contain", TODO_ITEM_THREE)
     })
 
     it("should trim text input [009]", function(){
@@ -123,7 +123,7 @@ describe("TodoMVC - React [000]", function(){
         .createTodo("    " + TODO_ITEM_ONE + "    ")
 
         // we use as explicit assertion here about the text instead of
-        // using cy.contains so we can specify the exact text of the element
+        // using 'contain' so we can specify the exact text of the element
         // does not have any whitespace around it
         .get(".todo-list li").eq(0).should("have.text", TODO_ITEM_ONE)
     })
@@ -138,8 +138,8 @@ describe("TodoMVC - React [000]", function(){
 
   context("Mark all as completed [00b]", function(){
     // New commands used here:
-    // - cy.check    https://github.com/cypress-io/cypress/wiki/check
-    // - cy.uncheck  https://github.com/cypress-io/cypress/wiki/uncheck
+    // - cy.check    http://on.cypress.io/check
+    // - cy.uncheck  http://on.cypress.io/uncheck
 
     beforeEach(function(){
       // This is an example of aliasing
@@ -201,7 +201,7 @@ describe("TodoMVC - React [000]", function(){
 
   context("Item [00g]", function(){
     // New commands used here:
-    // - cy.clear    https://github.com/cypress-io/cypress/wiki/clear
+    // - cy.clear    http://on.cypress.io/clear
 
     it("should allow me to mark items as complete [00f]", function(){
       cy
@@ -251,15 +251,15 @@ describe("TodoMVC - React [000]", function(){
           .type("buy some sausages").type("{enter}")
 
         // explicitly assert about the text value
-        .get("@todos").eq(0).should("have.text", TODO_ITEM_ONE)
-        .get("@secondTodo").should("have.text", "buy some sausages")
-        .get("@todos").eq(2).should("have.text", TODO_ITEM_THREE)
+        .get("@todos").eq(0).should("contain", TODO_ITEM_ONE)
+        .get("@secondTodo").should("contain", "buy some sausages")
+        .get("@todos").eq(2).should("contain", TODO_ITEM_THREE)
     })
   })
 
   context("Editing [00j]", function(){
     // New commands used here:
-    // - cy.blur    https://github.com/cypress-io/cypress/wiki/blur
+    // - cy.blur    http://on.cypress.io/blur
 
     beforeEach(function(){
       cy.createDefaultTodos().as("todos")
@@ -290,9 +290,9 @@ describe("TodoMVC - React [000]", function(){
           // could do that its just more mental work
           .blur()
 
-        .get("@todos").eq(0).should("have.text", TODO_ITEM_ONE)
-        .get("@secondTodo").should("have.text", "buy some sausages")
-        .get("@todos").eq(2).should("have.text", TODO_ITEM_THREE)
+        .get("@todos").eq(0).should("contain", TODO_ITEM_ONE)
+        .get("@secondTodo").should("contain", "buy some sausages")
+        .get("@todos").eq(2).should("contain", TODO_ITEM_THREE)
     })
 
     it("should trim entered text [00m]", function(){
@@ -304,9 +304,9 @@ describe("TodoMVC - React [000]", function(){
           .find(".edit").clear()
           .type("    buy some sausages    ").type("{enter}")
 
-        .get("@todos").eq(0).should("have.text", TODO_ITEM_ONE)
-        .get("@secondTodo").should("have.text", "buy some sausages")
-        .get("@todos").eq(2).should("have.text", TODO_ITEM_THREE)
+        .get("@todos").eq(0).should("contain", TODO_ITEM_ONE)
+        .get("@secondTodo").should("contain", "buy some sausages")
+        .get("@todos").eq(2).should("contain", TODO_ITEM_THREE)
     })
 
     it("should remove the item if an empty text string was entered [00n]", function(){
@@ -328,9 +328,9 @@ describe("TodoMVC - React [000]", function(){
         .get("@secondTodo")
           .find(".edit").clear().type("foo{esc}")
 
-        .get("@todos").eq(0).should("have.text", TODO_ITEM_ONE)
-        .get("@todos").eq(1).should("have.text", TODO_ITEM_TWO)
-        .get("@todos").eq(2).should("have.text", TODO_ITEM_THREE)
+        .get("@todos").eq(0).should("contain", TODO_ITEM_ONE)
+        .get("@todos").eq(1).should("contain", TODO_ITEM_TWO)
+        .get("@todos").eq(2).should("contain", TODO_ITEM_THREE)
     })
   })
 
@@ -353,7 +353,6 @@ describe("TodoMVC - React [000]", function(){
       cy
         .get("@todos").eq(0).find(".toggle").check()
         .get(".clear-completed").contains("Clear completed")
-
     })
 
     it("should remove completed items when clicked [00t]", function(){
@@ -361,14 +360,14 @@ describe("TodoMVC - React [000]", function(){
         .get("@todos").eq(1).find(".toggle").check()
         .get(".clear-completed").click()
         .get("@todos").should("have.length", 2)
-        .get("@todos").eq(0).should("have.text", TODO_ITEM_ONE)
-        .get("@todos").eq(1).should("have.text", TODO_ITEM_THREE)
+        .get("@todos").eq(0).should("contain", TODO_ITEM_ONE)
+        .get("@todos").eq(1).should("contain", TODO_ITEM_THREE)
     })
 
     it("should be hidden when there are no items that are completed [00u]", function(){
       cy
         .get("@todos").eq(1).find(".toggle").check()
-        .get(".clear-completed").click()
+        .get(".clear-completed").should("be.visible").click()
         .get(".clear-completed").should("not.exist")
     })
   })
@@ -379,8 +378,8 @@ describe("TodoMVC - React [000]", function(){
       // by writing out this function
       function testState() {
         cy
-          .get("@firstTodo").should("have.text", TODO_ITEM_ONE).and("have.class", "completed")
-          .get("@secondTodo").should("have.text", TODO_ITEM_TWO).and("not.have.class", "completed")
+          .get("@firstTodo").should("contain", TODO_ITEM_ONE).and("have.class", "completed")
+          .get("@secondTodo").should("contain", TODO_ITEM_TWO).and("not.have.class", "completed")
       }
 
       cy
@@ -400,10 +399,10 @@ describe("TodoMVC - React [000]", function(){
 
   context("Routing [00x]", function(){
     // New commands used here:
-    // - cy.window  https://github.com/cypress-io/cypress/wiki/window
-    // - cy.its     https://github.com/cypress-io/cypress/wiki/its
-    // - cy.invoke  https://github.com/cypress-io/cypress/wiki/invoke
-    // - cy.within  https://github.com/cypress-io/cypress/wiki/within
+    // - cy.window  http://on.cypress.io/window
+    // - cy.its     http://on.cypress.io/its
+    // - cy.invoke  http://on.cypress.io/invoke
+    // - cy.within  http://on.cypress.io/within
 
     beforeEach(function(){
       cy.createDefaultTodos().as("todos")
@@ -413,26 +412,19 @@ describe("TodoMVC - React [000]", function(){
       cy
         .get("@todos").eq(1).find(".toggle").check()
         .get(".filters").contains("Active").click()
-        .get("@todos").eq(0).should("have.text", TODO_ITEM_ONE)
-        .get("@todos").eq(1).should("have.text", TODO_ITEM_THREE)
+        .get("@todos").eq(0).should("contain", TODO_ITEM_ONE)
+        .get("@todos").eq(1).should("contain", TODO_ITEM_THREE)
     })
 
     it("should respect the back button [00z]", function(){
-      // TODO need to add the `cy.navigate` command here
-      // which handles both history navigation and url / hash
-      // navigation
-      // this test will indeterminately fail if your CPU is
-      // under load because window.history.back() is async.
-      // once the cy.navigate command is created this will fix
-      // this issue. for the moment let's just add a manual wait
       cy
         .get("@todos").eq(1).find(".toggle").check()
         .get(".filters").contains("Active").click()
         .get(".filters").contains("Completed").click()
         .get("@todos").should("have.length", 1)
-        .window().its("history").invoke("back").wait(200)
+        .go("back")
         .get("@todos").should("have.length", 2)
-        .window().its("history").invoke("back").wait(200)
+        .go("back")
         .get("@todos").should("have.length", 3)
     })
 
