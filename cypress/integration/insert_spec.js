012a75f391bfff207a8d6a086cafd23f35381dae
Brian Mann <brian@backbonerails.com>
2017-10-05 21:10:07 -0400

    remove additional file causing failures due to root beforeEach hook
    - NOTE: this is why we need to run spec files in isolation

--- cypress/integration/insert_spec.js
+++
@@ -1,7 +0,0 @@
-beforeEach(() => {
-  cy.visit('http://localhost:8888')
-})
-it('can insert new todo', () => {
-  cy.get('.new-todo').type('first todo{enter}')
-  cy.get('ul.todo-list').find('li').should('have.length', 1)
-})
