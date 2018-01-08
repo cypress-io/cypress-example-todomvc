444042e20d6241f556d59753ee6c7abd6a57b245
Jennifer Shehane <shehane.jennifer@gmail.com>
2018-01-08 10:30:51 -0500

    disable unused-vars linting for plugins index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,7 +11,9 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+/* eslint-disable no-unused-vars */
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
+/* eslint-enable no-unused-vars */
