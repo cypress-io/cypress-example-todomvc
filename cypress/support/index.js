0aafeecfe256d2dba4ab64137ef1a3e53ba1b083
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-05-10 10:42:26 -0400

    wrote custom selector code to select LI elements slightly better

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 // ***********************************************************
 // This example support/index.js is processed and
 // loaded automatically before your other test files.
@@ -21,3 +23,74 @@
 // Alternatively you can use CommonJS syntax:
 require('./commands')
 require('./defaults')
+
+// adjust Selector Playground preferences
+const getParentLI = ($el) => {
+  if (!$el.parent()) {
+    return
+  }
+
+  if ($el.parent().prop('tagName') === 'LI') {
+    return $el.parent()
+  }
+
+  if (!$el.parent().parent()) {
+    return
+  }
+
+  if (
+    $el
+    .parent()
+    .parent()
+    .prop('tagName') === 'LI'
+  ) {
+    return $el.parent().parent()
+  }
+}
+
+Cypress.SelectorPlayground.defaults({
+  onElement ($el) {
+    console.log('get selector for element', $el)
+
+    if ($el.prop('tagName') === 'INPUT') {
+      // input elements are probably a bad candidate for list
+      // selection
+      return
+    }
+
+    const $parentLi = getParentLI($el)
+
+    if (!$parentLi) {
+      // element is "regular" element, not part of the list
+      // return defaultSelector($el)
+      return
+    }
+
+    // the current element is part of the list!
+    console.log('element is part of the list', $parentLi)
+
+    // $parentLi points at the <li> elements
+    // now we need a good selector for the list itself
+    // which can be <ul>, <ol>, etc
+    const $list = $parentLi.parent()
+
+    if (!$list) {
+      console.log('could not find parent of LI')
+
+      return
+    }
+
+    if ($list.prop('class')) {
+      const selector =
+        $list.prop('tagName') +
+        '.' +
+        $list.prop('class') +
+        ' > ' +
+        $parentLi.prop('tagName')
+
+      console.log('list item selector', selector)
+
+      return selector
+    }
+  }
+})
