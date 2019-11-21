f4c1228427190cacafefb33e34a0a329a90ed0c0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-11-21 08:50:45 -0800

    escape for older versions of Cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -3,6 +3,11 @@ const findBraveBrowser = require('./find-brave')
 
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
+  if (!config.browsers) {
+    // we are running Cypress < v3.7.0 thus we cannot modify list of browsers
+    return
+  }
+
   return Promise.all([findEdgeBrowser(), findBraveBrowser()]).then(([edge, brave]) => {
     return {
       browsers: config.browsers.concat(edge, brave),
