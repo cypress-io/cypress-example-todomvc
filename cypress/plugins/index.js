584cc1998739270a1fa1f5eeb09b79a810554cc0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-11-21 08:40:56 -0800

    add edge and brave detection

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,19 +1,11 @@
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
+const findEdgeBrowser = require('./find-edge')
+const findBraveBrowser = require('./find-brave')
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
-/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  return Promise.all([findEdgeBrowser(), findBraveBrowser()]).then(([edge, brave]) => {
+    return {
+      browsers: config.browsers.concat(edge, brave),
+    }
+  })
 }
-/* eslint-enable no-unused-vars */
