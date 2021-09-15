0a29815dbb52851726f14499638e12486c2b9443
Yifeng Wang <i@ewind.us>
2021-09-15 17:01:16 +0800

    fix: docs link

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,7 +8,7 @@
 // commands for use throughout your tests.
 //
 // You can read more about custom commands here:
-// https://on.cypress.io/commands
+// https://docs.cypress.io/api/cypress-api/custom-commands
 // ***********************************************
 
 Cypress.Commands.add('createDefaultTodos', function () {
