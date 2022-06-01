85e288056159776f70e95583b52e6513fa3b0b59
Zach Bloomquist <git@chary.us>
2022-06-01 15:00:50 -0400

    remove comment

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   fixturesFolder: false,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {},
     baseUrl: 'http://localhost:8888',
   },
