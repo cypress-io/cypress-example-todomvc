5f784dc28140b80bfbf090a2c8183e27249229a1
Kevin Lam <kevnivek303@yahoo.com>
2023-08-01 08:58:14 -0400

    uncommented

--- cypress/e2e/test-gi.cy.js
+++ cypress/e2e/test-gi.cy.js
@@ -22,20 +22,51 @@ describe("Todo App", () => {
       cy.get(".todo-list li").eq(index).find("label").should("contain", todoText);
     });
 
-    // it("should mark 'Learn Cypress' as complete", function () {
-    //   cy.get(".todo-list li").contains("Learn Cypress").find(".toggle").check();
+    it("should mark 'Learn Cypress' as complete", function () {
+      cy.get(".todo-list li").contains("Learn Cypress").find(".toggle").check();
 
-    //   cy.get(".todo-list li")
-    //     .contains("Learn Cypress")
-    //     .should("have.class", "completed");
-    // });
+      cy.get(".todo-list li")
+        .contains("Learn Cypress")
+        .should("have.class", "completed");
+    });
 
-    // it("should validate the text of all items", function () {
-    //   cy.get(".todo-list li").should("have.length", 3);
-    //   cy.validateTodoItem(0, "Make every second count");
-    //   cy.validateTodoItem(1, "Invest in yourself");
-    //   cy.validateTodoItem(2, "Learn Cypress");
-    // });
+    it("should validate the text of all items", function () {
+      cy.get(".todo-list li").should("have.length", 3);
+      cy.validateTodoItem(0, "Make every second count");
+      cy.validateTodoItem(1, "Invest in yourself");
+      cy.validateTodoItem(2, "Learn Cypress");
+    });
   });
 });
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// describe("Testing the to do items", () => {
+//   let TODO_ITEM_ONE = "Make every second count";
+//   let TODO_ITEM_TWO = "Invest in yourself";
+//   let TODO_ITEM_THREE = "Learn Cypress";
+
+//   beforeEach(() => {
+//     cy.visit("http://localhost:8888/#/");
+//   });
+
+//   it("displays items", () => {
+//     cy.get(".new-todo").type(TODO_ITEM_ONE).type("{enter}");
+//     cy.get(".new-todo").type(TODO_ITEM_TWO).type("{enter}");
+//     cy.get(".new-todo").type(TODO_ITEM_THREE).type("{enter}");
+//   });
+// });
+
+// context("Mark all as completed", function () {
+//   beforeEach(function () {
+//     cy.createDefaultTodos().as("todos");
+//   });
+
+//   it("should allow me to mark all items as completed", function () {
+//     cy.get(".toggle-all").check();
+
+//     cy.get("@todos").eq(0).should("have.class", "completed");
+
+//     cy.get("@todos").eq(1).should("have.class", "completed");
+
+//     cy.get("@todos").eq(2).should("have.class", "completed");
+//   });
+// });
